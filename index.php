6d8cea4211eb393ffb94276c4c9270048f3d9979
bykovsky <a.v.bykovsky@gmail.com>
2024-05-16 13:57:00 +0300

    index.php and robots.txt

--- index.php
+++ index.php
@@ -2,11 +2,16 @@
 require_once('inc/header.inc.php');
 ?>
 
-<section class="section">
+<section class="section main-content">
     <div class="container">
         <div class="row">
-            <div class="col-12">
-              section template
+            <div class="col-md-6">
+              <h2 class="main-content__title">Быковский Александр Викторович</h2>
+              <div class="main-content__content-block">
+              <p>Меня зовут Быковский Александр Виктрорович. Мне полных 43&nbsp;года, я&nbsp;проживаю в&nbsp;городе Орле</p>
+              <p>Я&nbsp;знаком с&nbsp;WEB-разработкой с&nbsp;2001&nbsp;года. Активно ей&nbsp;занимаюсь с&nbsp;2014 года</p>
+              <p>В&nbsp;связи с&nbsp;бурным развитем импортозамещения появилась необходимость взяться за&nbsp;изучение 1С&nbsp;BITRIX</p>
+              </div>
             </div>
         </div>
     </div>
